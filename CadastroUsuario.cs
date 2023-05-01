d075222a0f7ffd89cd4ecaec18af0a0949f2e278
Eduardo Carvalho Smith <111188049+Eduardocsmith21@users.noreply.github.com>
2023-04-30 22:55:44 -0400

    Create CadastroUsuario.cs

---
+++ CadastroUsuario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaLogin
+{
+    static class CadastroUsuario
+    {
+        private static Usuario[] usuarios =
+        {
+            new Usuario(){login = "Eduardo", senha = "abc123"},
+            new Usuario(){login = "Juca", senha = "123a"},
+            new Usuario(){login = "rodolfo", senha = "rd12"}
+        };
+        private static Usuario _userLog = null;
+        public static Usuario Userlog
+        {
+            get { return _userLog; }
+            private set { _userLog = value; }
+        }
+        public static bool Login (string login, string senha)
+        {
+            foreach(Usuario usuario in usuarios)
+            {
+                if(usuario.login == login && usuario.senha == senha)
+                {
+                    Userlog = usuario;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}

73ce642224b91378ccd035c70759654a8e00cb31
Eduardo Carvalho Smith <111188049+Eduardocsmith21@users.noreply.github.com>
2023-04-30 22:55:54 -0400

    Create FormLogin.cs

---
+++ FormLogin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaLogin
+{
+    public partial class FormLogin : Form
+    {
+        public static bool Cancelar = false;
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            string usu = texUsuario.Text;
+            string sen = texSen.Text;
+            if (CadastroUsuario.Login(usu, sen))
+            {
+                Close(); //Fechar Forms
+            }
+            else
+            {
+                MessageBox.Show("Acesso negado!");
+                texUsuario.Text = "";
+                texSen.Text = "";
+                texUsuario.Focus();
+                Close();
+            }   
+        }
+
+        private void btnCance_Click(object sender, EventArgs e)
+        {
+            Cancelar = true;
+            Close();
+        }
+    }
+}

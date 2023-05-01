38be0d2e2805392827b900582a5109102a74ce2c
Eduardo Carvalho Smith <111188049+Eduardocsmith21@users.noreply.github.com>
2023-04-30 22:56:05 -0400

    Create Usuario.cs

---
+++ Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaLogin
+{
+    internal class Usuario
+    {
+        public string login { get; set; }
+        public string senha { get; set; }
+    }
+}

e317ac9806e6d85539f9635e8e226bac062d83d0
Eduardo Carvalho Smith <111188049+Eduardocsmith21@users.noreply.github.com>
2023-04-30 22:55:51 -0400

    Create Form1.Designer.cs

---
+++ Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace SistemaLogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_boasVindas = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label_boasVindas
+            // 
+            this.label_boasVindas.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_boasVindas.Location = new System.Drawing.Point(179, 150);
+            this.label_boasVindas.Name = "label_boasVindas";
+            this.label_boasVindas.Size = new System.Drawing.Size(442, 150);
+            this.label_boasVindas.TabIndex = 0;
+            this.label_boasVindas.Text = "Tela Inicial\r\n";
+            this.label_boasVindas.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_boasVindas);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inicio";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_boasVindas;
+    }
+}
+
